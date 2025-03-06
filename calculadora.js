0720da0ab5240770fd2766975a223c51e5620f96
Heloisa31 <heloisa.souza.pinto@escola.pr.gov.br>
2025-03-06 07:19:58 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,32 @@
+function adicao (num1, num2) {
+    return num1 + num2
+}
+
+
+function subtracao (num1, num2) {
+    return num1 - num2
+}
+
+
+function multiplicacao (num1, num2) {
+    return num1 * num2
+}
+
+
+function divisao (num1, num2) {
+   return num1 / num2
+}
+
+
+function restodivisao (num1, num2) {
+   return num1 % num2
+}
+
+
+console.log ("O resultado da adição é: ", adicao (4, 5))
+console.log ("O resultado da subtração é: ", subtracao (7, 4))
+console.log ("O resultado da multiplicação é: ", multiplicacao (6, 8))
+console.log ("O resultado da divisão é: ", divisao (20, 2))
+console.log ("O resto da divisão é: ", restodivisao (20, 2))
+
+
